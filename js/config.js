9bac58f43fa294aa58dcc6923f687ca07e7451ed
Raghu Srungavarapu <jsru2@pdx.edu>
2021-03-12 10:47:32 -0800

    pointed signup/signin to cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_1ncez8Oic', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ovrla1cvolmm4q4q41fbnp655', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://wildrydes.intranet201.bizdevworks.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
