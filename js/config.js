795b31abcbfec802f0646619c2f1a2e824396616
Raghu Srungavarapu <jsru2@pdx.edu>
2021-03-12 14:20:25 -0800

    backend integrated

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://wildrydes.intranet201.bizdevworks.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://pmocdxqd57.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
